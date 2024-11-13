dafe511f5bc5379633c09fdf996ce6cd5e763e14
rohta_linux <ootar0121@icloud.com>
2024-11-13 15:45:56 +0900

    add_printf_and_gnl

--- libft.h
+++ libft.h
@@ -6,13 +6,17 @@
 /*   By: rohta <rohta@student.42.jp>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 05:00:19 by rohta             #+#    #+#             */
-/*   Updated: 2024/11/12 14:12:57 by rohta            ###   ########.fr       */
+/*   Updated: 2024/11/13 15:36:58 by rohta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdbool.h>
 # include <unistd.h>
 # include <stddef.h>
 # include <stdlib.h>
@@ -46,7 +50,7 @@ void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int			ft_atoi(const char *str);
-long			ft_long_atoi(const char *str);
+long		ft_long_atoi(const char *str);
 void		*ft_calloc(size_t count, size_t size);
 char		*ft_strdup(const char *s1);
 
@@ -71,5 +75,78 @@ void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+char		*get_next_line(int fd);
+char		*get_next_line_bonus(int fd);
+
+# ifndef PRINTFF_NULL
+#  define PRINTF_NULL -1
+# endif
+
+# ifndef STDOUT_FD
+#  define STDOUT_FD 1
+# endif
+
+# ifndef NOT_SPEC
+#  define NOT_SPEC -1
+# endif
+
+# ifndef FLAGS
+#  define FLAGS "-+ 0#"
+# endif
+
+# ifndef DESIMALS
+#  define DESIMALS "0123456789"
+# endif
+
+# ifndef LOW_HEX
+#  define LOW_HEX "0123456789abcdef"
+# endif
+
+# ifndef UP_HEX
+#  define UP_HEX "0123456789ABCDEF"
+# endif
+
+typedef struct s_flags
+{
+	bool	flag_minus;
+	bool	flag_plus;
+	bool	flag_space;
+	bool	flag_zero;
+	bool	flag_hashtag;
+}	t_flags;
+
+typedef struct s_params
+{
+	t_flags	*flags;
+	ssize_t	*width;
+	ssize_t	*precision;
+	char	*converted;
+	char	specifier;
+	bool	dot;
+}	t_params;
+
+int		ft_printf(const char *format, ...);
+char	*ft_printf_check(t_params *params, char *format, va_list args);
+void	ft_init_params(t_params *params);
+char	*ft_apply_character(int arg);
+char	*ft_apply_string(char *arg);
+char	*ft_apply_pointer(size_t arg);
+char	*ft_apply_desimals(long long arg, size_t base_len, char *base);
+char	*ft_apply_hex(unsigned long long arg, size_t base_len, char *base);
+size_t	ft_write_number(t_params *params);
+size_t	ft_write_unsigned(t_params *params);
+size_t	ft_write_hex(t_params *params);
+ssize_t	ft_write_string(t_params *params);
+size_t	ft_write_pointer(t_params *params);
+int		ft_zero_check(t_params *params);
+size_t	ft_write_prec(t_params *params, size_t put_prec, size_t *print_len,
+			size_t conv_len);
+size_t	ft_write_width(t_params *params, size_t put_prec, size_t conv_len,
+			size_t *print_len);
+void	ft_conv_prec(t_params *params, ssize_t conv_len, size_t *put_prec,
+			size_t *print_len);
+void	ft_prec_dot(t_params *params, size_t *print_len, size_t *put_prec,
+			int number);
+void	ft_minus_tmp(t_params *params, size_t *conv_len);
 
 #endif
